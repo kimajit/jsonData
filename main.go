39b64afe5162d3c6e7e2afa3856e25b21f3f1fde
Mahesh Balli <mahesh.balli@relanto.ai>
2022-11-28 15:14:50 +0530

    third commit

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+type comments struct {
+	Comments []comment `json:"comments"`
+}
+
+type comment struct {
+	ID     int    `json:"id"`
+	Body   string `json:"body"`
+	PostId int    `json:"postid"`
+}
 
 func main() {
 	resp, err := http.Get("https://my-json-server.typicode.com/typicode/demo/db")
@@ -21,5 +33,17 @@ func main() {
 	log.Print(sb)
 
 	_ = ioutil.WriteFile("file.json", body, 0644)
+
+	//********************************************//
+
+	//var x comments
+	file, _ := ioutil.ReadFile("file.json")
+	data := comments{}
+	_ = json.Unmarshal(file, &data)
+	for i := 0; i < len(data.Comments); i++ {
+		fmt.Println("comments ID: ", data.Comments[i].ID)
+		fmt.Println("Body: ", data.Comments[i].Body)
+		fmt.Println("PostId: ", data.Comments[i].PostId)
+	}
 
 }
